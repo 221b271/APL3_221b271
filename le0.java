ad518dbbcc7d645014873d08b7c792562fc66c3e
221B271 <221B271@juet.co.in>
2025-01-17 14:55:21 +0530

    First question

---
+++ le0.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class le0
+{
+	public static void main(String[] args) {
+	    int x = Integer.parseInt(args[0]);
+	    for(int i=1;i<=10;i++) System.out.println(x + " x " + i + " = " + x*i);
+		//System.out.println("Hello World");
+	}
+}
